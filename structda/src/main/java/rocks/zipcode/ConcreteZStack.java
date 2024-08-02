939140007c94b444da2f58074d0860d745db0918
igordon215 <igordon215@gmail.com>
2024-08-02 13:59:16 -0400

    zStack

--- structda/src/main/java/rocks/zipcode/ConcreteZStack.java
+++ structda/src/main/java/rocks/zipcode/ConcreteZStack.java
@@ -1,35 +1,38 @@
 package rocks.zipcode;
 
-public class ConcreteZStack<E> implements ZStack<E>{
+import java.util.ArrayDeque;
+
+public class ConcreteZStack<E> implements ZStack<E> {
+
+    ArrayDeque<E> myDeque = new ArrayDeque<>();
 
     @Override
     public void push(E item) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'push'");
+        myDeque.push(item);
     }
 
     @Override
     public E pop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'pop'");
+        return myDeque.pop();
     }
 
     @Override
     public E peek() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+        return myDeque.peek();
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isEmpty'");
+        if (myDeque.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
     public int size() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'size'");
+        return myDeque.size();
     }
 
 }
